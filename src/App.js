7edf7e6ad3ecd11c6ad1d8a81233ed0e32dd2e20
RosyCella <88027880+RosyCella@users.noreply.github.com>
2021-11-23 14:05:58 +1100

    created react weather files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1> React Weather App </h1>
+      <footer>
+        Coded by
+        <a href="https://www.rosycella.com" target="_blank" rel="noreferrer">
+          Rosy Cella{" "}
         </a>
-      </header>
+        and is
+        <a href="" target="_blank" rel="noreferrer">
+          {" "}
+          open-sourced on GitHub{" "}
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
